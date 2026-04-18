3a28a6f89324a076a0630d4e81cb6e41a25217fa
zyouime <igorneizvestny69@gmail.com>
2026-04-18 19:24:31 +0300

    callback

--- src/main/java/me/zyouime/configutil/setting/Setting.java
+++ src/main/java/me/zyouime/configutil/setting/Setting.java
@@ -9,20 +9,26 @@ import me.zyouime.configutil.ModConfig;
 
 import java.io.File;
 import java.lang.reflect.Type;
+import java.util.function.Consumer;
 
 @Getter
 public class Setting<T> {
 
-    @Setter
     private T value;
     private final T defaultValue;
     private final String configKey;
     private final Type type;
+    @Setter private Consumer<T> setCallback;
 
-    public Setting(String configKey, Type type, T defaultValue) {
+    public Setting(String configKey, Type type, T defaultValue, Consumer<T> setCallback) {
         this.configKey = configKey;
         this.defaultValue = defaultValue;
         this.type = type;
+        this.setCallback = setCallback;
+    }
+
+    public Setting(String configKey, Type type, T defaultValue) {
+        this(configKey, type, defaultValue, null);
     }
 
     public void initValue(JsonElement value, Gson gson) {
@@ -33,6 +39,13 @@ public class Setting<T> {
         JsonObject object = ModConfig.loadConfig(configFile, gson);
         object.add(configKey, gson.toJsonTree(this.value, this.type));
         ModConfig.saveConfig(object, configFile, gson);
+    }
+
+    public void setValue(T value) {
+        if (setCallback != null) {
+            setCallback.accept(value);
+        }
+        this.value = value;
     }
 
     public void reset() {
